e4d4ee7407df4b776425e7e26859f4e8f2f2ebca
AleksanderGm <aleksgrimo@hotmail.com>
2023-09-04 00:08:02 +0200

    style.css

---
+++ Module2/index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "style.css" />
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Module 1 IS-115</h1>
+    <a class="button" href="task_2.php">Module 1 - 2</a>
+    <a class="button" href="task_3.php">Module 1 - 3</a>
+    <a class="button" href="task_4.php">Module 1 - 4</a>
+    <a class="button" href="task_5.php">Module 1 - 5</a>
+</body>
+</html>
