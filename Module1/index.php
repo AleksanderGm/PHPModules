527e68c3fcf16a78b69105a0ec26f6ed2ada46d0
aleksandergm <aleksandgm@uia.no>
2023-09-04 00:11:08 +0200

    smallfixCSS

--- Module1/index.php
+++ Module1/index.php
@@ -1,18 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = "style.css" />
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
-<body>
+<body id="container">
 
-<h1>Module 1 IS-115</h1>
-    <a class="button" href="task_2.php">Module 1 - 2</a>
-    <a class="button" href="task_3.php">Module 1 - 3</a>
-    <a class="button" href="task_4.php">Module 1 - 4</a>
-    <a class="button" href="task_5.php">Module 1 - 5</a>
+<div class="middiv">
+    <h1>Modul 1</h1>
+    <div class="btndiv">
+        <a href="task_1.php" class="btn">Oppgave 1</a>
+        <a href="task_1.php" class="btn">Oppgave 2</a>
+        <a href="task_1.php" class="btn">Oppgave 3</a>
+        <a href="task_1.php" class="btn">Oppgave 4</a>
+
+    </div>
+</div>
+
 </body>
 </html>
